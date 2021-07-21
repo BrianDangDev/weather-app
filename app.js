8b2be4e606a4d2cfd874843ccbe29cd2a0aefe91
Broiandang <binhcdang.bd@gmail.com>
2021-07-21 14:01:35 +0700

    connected to api and improve UI

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 const notificationElement = document.querySelector(".notification");
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
-const descElement = document.querySelector("temperature-description p");
-const locationElement = document.querySelector("location p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
 
 const weather = {};
 weather.temperature = {
@@ -27,37 +27,37 @@ if('geolocation' in navigator){
 
 //set user position
 function setPosition(position){
-    let latitute = position.coords.latitute;
-    let longtitute = position.coords.longtitute;
-    getWeather(latitute, longtitute);
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    getWeather(latitude, longitude);
 }
 function showError(error){
 notificationElement.style.display = "block";
 notificationElement.innerHTML = `<p>${error.message}</p>`;
 }
 
-function getWeather(latitute, longtitute){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitute}&lon=${longtitute}&appid=${key}`;
-    fetch(api)
-        .then(function(response){
-            let data = response.json();
-            return data;
-        })
-        .then(function(data){
-            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-            weather.description = data.weather[0].description;
-            weather.iconId = data.weather[0].icon;
-            weather.city = data.name;
-            weather.country = data.sys.country;
-        })
-        .then(function(){
-            displayWeather();
-        });
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+fetch(api)
+.then(function(response){
+    let data = response.json();
+    return data;
+})
+.then(function(data){
+    weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+    weather.description = data.weather[0].description;
+    weather.iconId = data.weather[0].icon;
+    weather.city = data.name;
+    weather.country = data.sys.country;
+})
+.then(function(){
+    displayWeather();
+})
 }
 
 function displayWeather(){
-iconElement.innerHTML = `<img src = icon${weather.iconId}.png"/>`;
-tempElement.innerHTML = `${weather.temperature.value} ° <span>C</span></p>`;
+iconElement.innerHTML = `<img src = "icons/${weather.iconId}.png"/>`;
+tempElement.innerHTML = `${weather.temperature.value} °<span>C</span></p>`;
 descElement.innerHTML = weather.description;
 locationElement.innerHTML =`${weather.city}, ${weather.country}`;
 }
